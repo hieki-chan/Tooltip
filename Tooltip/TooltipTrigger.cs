80a95e5e031939c5a66bb493de204bf462ccc17e
@hieki_chan <92980856+hieki-chan@users.noreply.github.com>
2023-09-06 22:15:08 +0700

    Add files via upload

---
+++ Tooltip/TooltipTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace HiekiTools.UI.Tooltip
+{
+    public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField, TextArea]
+        string mDisplayText;
+
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            Tooltip.Instance.SetTooltip(mDisplayText);
+        }
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Tooltip.Instance.HideTooltip();
+        }
+
+        public void SetTooltip(string aText)
+        {
+            mDisplayText = aText;
+        }
+    }
+}
